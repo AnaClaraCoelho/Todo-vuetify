d45cb3fd1562d5c355608b7948ef331bb06f151c
Seu Nome <voce@exemplo.com>
2022-09-15 15:42:10 -0300

    "Criação da página de registro"

--- src/views/RegistroView.vue
+++ src/views/RegistroView.vue
@@ -4,11 +4,13 @@
       <v-row class="d-flex flex-column align-center">
         <v-col cols="12" md="4">
           <v-text-field
-            v-model="Usuario"
+            v-model="usuario"
             :rules="regrasUser"
             :counter="10"
             label="Nome de usuario"
             required
+            @input="$v.usuario.$touch()"
+            @blur="$v.usuario.$touch()"
           ></v-text-field>
         </v-col>
         <v-col cols="12" md="4">
@@ -17,19 +19,26 @@
             :rules="emailRules"
             label="E-mail"
             required
+            @input="$v.email.$touch()"
+            @blur="$v.email.$touch()"
           ></v-text-field>
         </v-col>
 
         <v-col cols="12" md="4">
           <v-text-field
             type="password"
-            v-model="Senha"
+            v-model="senha"
             :rules="passwordRules"
             :counter="10"
             label="Senha"
             required
+            @input="$v.senha.$touch()"
+            @blur="$v.senha.$touch()"
           ></v-text-field>
         </v-col>
+        <v-btn class="justify-center" color="primary" rounded @click="submit">
+          Registrar-se
+        </v-btn>
       </v-row>
     </v-container>
   </v-form>
@@ -40,7 +49,7 @@ export default {
   name: "RegistroView",
   data: () => ({
     valid: false,
-    Usuario: "",
+    usuario: "",
     regrasUser: [
       (v) => !!v || "É obrigatório ter um nome de usuario",
       (v) => v.length <= 10 || "O nome não pode ultrapassar 10 caracteres!",
@@ -50,10 +59,18 @@ export default {
       (v) => !!v || "É obrigatório ter um E-mail",
       (v) => /.+@.+/.test(v) || "O E-mail não é valido",
     ],
-    Senha: "",
+    senha: "",
     passwordRules: [
       (v) => v.length >= 8 || "A senha precisa conter entre 8 e 10 caracteres",
     ],
   }),
+  methods: {
+    submit() {
+      this.$v.$touch();
+      if (!this.usuario == "" || !this.senha == "" || !this.email == "") {
+        this.$router.push({ name: "login" });
+      }
+    },
+  },
 };
 </script>
